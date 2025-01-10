30505b68dde95bf9ba23a951a4265a97d188d42c
joao.murari <joao_pichula@hotmail.com>
2025-01-10 15:06:05 -0300

    /feat: Personal exception classes created

--- src/main/java/exception/ProductUnavailableException.java
+++ src/main/java/exception/ProductUnavailableException.java
@@ -3,7 +3,7 @@ package exception;
 public class ProductUnavailableException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
-	public ProductUnavailableException(String message) {
-        super(message);
+	public ProductUnavailableException(String msg) {
+        super(msg);
     }
 }

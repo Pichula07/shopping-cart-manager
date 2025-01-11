f05bf810b30ae4d939fd421bf67d4696098374ba
joao.murari <joao_pichula@hotmail.com>
2025-01-11 18:50:51 -0300

    /feat: add main application entry point

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Locale;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -8,16 +10,23 @@ import controller.CartController;
 import controller.ProductController;
 import dao.CartDao;
 import dao.ProductDao;
-import entity.Product;
 import view.MenuView;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		Locale.setDefault(Locale.US);
+		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("shopping_cart");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		ProductDao productDao = new ProductDao(entityManager);
+		CartDao cartDao = new CartDao(entityManager);
+		ProductController productController = new ProductController(productDao);
+		CartController cartController = new CartController(cartDao, productDao);
+		MenuView menuView = new MenuView(productController, cartController);
 
-	
+        menuView.displayMenu();
 	}
+
 }
